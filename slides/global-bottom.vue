6a9ac33df6f402a3e3f971775b334cd9d55e8975
Enrique Gonzalez Paredes <enriqueg@cscs.ch>
2025-03-06 23:34:28 +0100

    Add slide setup using sli.dev
    It requires a node/npm installation. To start the development server just `npm run dev` from the slides folder.
    
    The current structure assumes every session goes into a different folder, and the session slides are included from the main `slides.md` file. The directory structure is explained in [sli.dev documentation](https://sli.dev/custom/directory-structure). Note that the asset files (e.g. images, ...) used in slides' frontmatter need to go in `/public` to be accessible (https://sli.dev/guide/faq#assets-handling), so I store there the theme backgrounds. Other resources used from the slide body can go in the session asset folder.
    
    The only theme customization added is the custom footer with a slide counter the ETH and CSCS logos defined in `global-botton.vue`. It is done in the most crude and basic way possible, due to extremely poor knowledge of vue, unoCSS and web and atomic css frameworks in general.
    
    More instructions here: https://sli.dev/guide/

---
+++ slides/global-bottom.vue
@@ -0,0 +1,6 @@
+<!-- global-bottom.vue -->
+<template>
+  <img src="./images/cscs-logo.png" class="absolute bottom-0 left-0 p-1 h-12" />
+  <img src="./images/eth-logo.png" class="absolute bottom-0 right-0 p-1 h-12" />
+  <footer class="absolute bottom-1 left-47% p-2">{{ $nav.currentPage }} / {{ $nav.total }}</footer>
+</template>
